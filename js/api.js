f3d2493ac9c83373d68625b6857539e7ac66cd4d
luzhaofeng <574317128@qq.com>
2021-07-19 18:19:47 +0800

    feat: 初始化

---
+++ js/api.js
@@ -0,0 +1,9 @@
+// 公共路径
+const baseUrl = 'http://120.78.230.188:9527/api/';
+// const baseUrl = 'http://localhost:9527/api/';
+// 获取一个内容
+const getContentUrl = baseUrl + 'content/randomGet';
+// 推荐
+const recommendUrl = baseUrl + 'recommend/recommend';
+// 不推荐
+const notRecommendUrl = baseUrl + 'recommend/notRecommend';
