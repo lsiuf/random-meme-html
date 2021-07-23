17f1a3e6fa04512ed8bc96f197c5d11f0d5fd0d8
luzhaofeng <574317128@qq.com>
2021-07-23 17:44:56 +0800

    feat: 1.新增全局图片路径变量；2.新增后台管理系统页面

---
+++ admin/js/api.js
@@ -0,0 +1,18 @@
+// 公共路径
+const baseUrl = 'http://120.78.230.188:9527/admin/';
+// 图片公共路径
+const baseImageUrl = 'http://120.78.230.188/images/';
+
+// 分页查询内容
+const pageContentUrl = baseUrl + 'content/page';
+// 更新内容状态
+const updateContentUrl = baseUrl + 'content/update';
+// 导入内容
+const importContentUrl = baseUrl + 'content/import';
+
+// 分页查询分类
+const pageClassifyUrl = baseUrl + 'classify/page';
+// 新增分类
+const insertClassifyUrl = baseUrl + 'classify/insert';
+// 更新分类
+const updateClassifyUrl = baseUrl + 'classify/update';
--- web/js/api.js
+++ web/js/api.js
@@ -1,6 +1,8 @@
 // 公共路径
 const baseUrl = 'http://120.78.230.188:9527/api/';
-// const baseUrl = 'http://localhost:9527/api/';
+// 图片公共路径
+const baseImageUrl = 'http://120.78.230.188/images/';
+
 // 获取一个内容
 const getContentUrl = baseUrl + 'content/randomGet';
 // 推荐
